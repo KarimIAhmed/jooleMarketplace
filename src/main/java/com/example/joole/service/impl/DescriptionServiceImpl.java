70c8c36307683fa0c35f210e2621d998ef350816
Tengyang Zhang <everlastinglight99@gmail.com>
2022-10-11 11:37:06 -0700

    update service

---
+++ src/main/java/com/example/joole/service/impl/DescriptionServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.joole.service.implementation;
+
+import com.example.joole.model.Description;
+import com.example.joole.repository.DescriptionRepository;
+import com.example.joole.service.DescriptionService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class DescriptionServiceImpl implements DescriptionService {
+    private DescriptionRepository descriptionRepository;
+
+    public Description saveDescription(Description description){
+        return descriptionRepository.save(description);
+    }
+
+    public List<Description> getProducts(){
+        return descriptionRepository.findAll();
+    }
+
+    public String deleteProduct(long id){
+        descriptionRepository.deleteById(id);
+        return "DescriptionService " + id + " has been removed";
+    }
+}
